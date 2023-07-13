504cd38ec1444b2421ace15631771d0f99718328
Sophin420 <krystian.bielowka@wp.pl>
2023-07-13 12:24:12 +0200

    quick theme

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import Navbar from './Navbar'
+import { useRouter } from 'next/router'
+
+const Layout = ({ children }) => {
+
+  const router = useRouter();
+
+  return (
+    <>
+      <Navbar />
+      <main id='main' style={{ WebkitTapHighlightColor: 'transparent', backgroundImage: router?.pathname === '/404' ? "url('/film_grain.png'), url('/white_grain.png')" : null }}>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default Layout
