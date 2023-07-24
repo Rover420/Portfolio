0696c2297d621e43c1fd43e3585b952c30bfff72
Sophin420 <krystian.bielowka@wp.pl>
2023-07-24 21:48:03 +0200

    X-Robots-Tag

--- next.config.js
+++ next.config.js
@@ -16,6 +16,21 @@ const nextConfig = withPWA({
     locales: [ 'pl', 'en' ],
     defaultLocale: 'en',
   },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  }
 })
 
 module.exports = nextConfig
