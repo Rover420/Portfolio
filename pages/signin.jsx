2c52c2133823afc6ce8ed000d10aa9969ce30e14
Sophin420 <krystian.bielowka@wp.pl>
2023-07-27 15:30:16 +0200

    initialProviders

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -13,18 +13,30 @@ export async function getStaticProps({ locale }) {
 
     const rawt = await require(`@/locale/${locale ?? 'en'}`);
 
-    const t = rawt.login
+    const t = rawt.signin
 
-    const initialProviders = [
-      {id: 'discord', name: 'Discord', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/discord', callbackUrl: 'http://localhost:3000/api/auth/callback/discord'},
-      {id: 'github', name: 'GitHub', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/github', callbackUrl: 'http://localhost:3000/api/auth/callback/github'},
-      {id: 'facebook', name: 'Facebook', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/facebook', callbackUrl: 'http://localhost:3000/api/auth/callback/facebook'},
-      {id: 'google', name: 'Google', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/google', callbackUrl: 'http://localhost:3000/api/auth/callback/google'}
-    ]
+    try {
+      const initialProviders = await getProviders();
+      
+      return {
+        props: { t, initialProviders: initialProviders ?? [] },
+        revalidate: 60,
+      };
+    } catch (e) {
   
-    return {
-      props: { t, initialProviders },
-      revalidate: 60,
-    };
+      console.error('Error fetching clicks data: ', e.message);
+
+      const initialProviders = [
+        {id: 'discord', name: 'Discord', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/discord', callbackUrl: 'http://localhost:3000/api/auth/callback/discord'},
+        {id: 'github', name: 'GitHub', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/github', callbackUrl: 'http://localhost:3000/api/auth/callback/github'},
+        {id: 'facebook', name: 'Facebook', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/facebook', callbackUrl: 'http://localhost:3000/api/auth/callback/facebook'},
+        {id: 'google', name: 'Google', type: 'oauth', signinUrl: 'http://localhost:3000/api/auth/signin/google', callbackUrl: 'http://localhost:3000/api/auth/callback/google'}
+      ]
+  
+      return {
+        props: { t, initialProviders },
+        revalidate: 60,
+      };
+    }
 }
   
